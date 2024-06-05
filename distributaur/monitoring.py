47d8d45a8b88c7925b1733c519a09476cddd584d
acbaez9 <97056049+acbaez9@users.noreply.github.com>
2024-06-04 19:17:41 -0700

    fixed flower bug

--- distributaur/monitoring.py
+++ distributaur/monitoring.py
@@ -5,13 +5,22 @@ import sys
 flower_processes = []
 
 
-def start_monitoring_server(task_name="example") -> None:
+def start_monitoring_server(task_name="example_worker") -> None:
     """
     Start Flower monitoring in a separate process.
     The monitoring process will be automatically terminated when the main process exits.
     """
     # get the current python process
-    flower_process = Popen([sys.executable, "-m", "flower", "--app", task_name])
+    flower_process = Popen(
+        [
+            "celery",
+            "-A",
+            {task_name},
+            "--broker=redis://default:lQXiTg6afNjUV60JN8QLAYOFYyq7YXZy@redis-17504.c289.us-west-1-2.ec2.redns.redis-cloud.com:17504/0",
+            "flower",
+        ]
+    )
+
     atexit.register(flower_process.terminate)
     flower_processes.append(flower_process)
 
