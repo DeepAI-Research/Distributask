2865eef89179892e0dbf9797075a55322ed0e8ca
acbaez9 <97056049+acbaez9@users.noreply.github.com>
2024-06-11 12:07:37 -0700

    added time tools

--- distributaur/example/distributed.py
+++ distributaur/example/distributed.py
@@ -88,3 +88,32 @@ if __name__ == "__main__":
             completed_tasks = sum([task.ready() for task in tasks])
             pbar.update(completed_tasks - pbar.n)
             time.sleep(1)
+
+
+
+    prev_tasks = 0
+    first_task_done = False
+    queue_start_time = time.time()
+    # Wait for the tasks to complete
+    print("Tasks submitted to queue. Initializing queue...")
+    with tqdm(total=len(tasks), unit="task") as pbar:
+        while not all(task.ready() for task in tasks):
+            completed_tasks = sum([task.ready() for task in tasks])
+            pbar.update(completed_tasks - pbar.n)
+
+            if completed_tasks > 0:
+                # begin estimation from time of first task
+                if not first_task_done:
+                    first_task_done = True
+                    first_task_start_time = time.time()
+                    print("Initialization completed. Tasks started...")
+
+                # calculate and print total elapsed time and estimated time left
+                end_time = time.time()
+                elapsed_time = end_time - first_task_start_time
+                time_per_tasks = elapsed_time / completed_tasks
+                time_left = time_per_tasks * (len(tasks) - completed_tasks)
+
+                pbar.set_postfix(elapsed=f"{elapsed_time:.2f}s", time_left=f"{time_left:.2f}")
+            # sleep for a few seconds
+            time.sleep(1)
