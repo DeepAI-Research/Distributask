9fbbb88fbada178dc140c386aa1229bf452c9993
acbaez9 <97056049+acbaez9@users.noreply.github.com>
2024-07-01 16:04:31 -0700

    moved task monitoring into distributaur object

--- distributaur/example/local.py
+++ distributaur/example/local.py
@@ -132,45 +132,4 @@ if __name__ == "__main__":
 
         atexit.register(kill_docker)
 
-    def tasks_done():
-        print("All tasks successfully completed.")
-
-    def cleanup_redis():
-        patterns = ["celery-task*", "task_status*"]
-        redis_connection = distributaur.get_redis_connection()
-        for pattern in patterns:
-            for key in redis_connection.scan_iter(match=pattern):
-                redis_connection.delete(key)
-
-    atexit.register(cleanup_redis)
-
-    prev_tasks = 0
-    first_task_done = False
-    queue_start_time = time.time()
-    # Wait for the tasks to complete
-    print("Tasks submitted to queue. Initializing queue...")
-    with tqdm(total=len(tasks), unit="task") as pbar:
-        while not all(task.ready() for task in tasks):
-            current_tasks = sum([task.ready() for task in tasks])
-            pbar.update(current_tasks - pbar.n)
-
-            if current_tasks > 0:
-                # begin estimation from time of first task
-                if not first_task_done:
-                    first_task_done = True
-                    first_task_start_time = time.time()
-                    print("Initialization completed. Tasks started...")
-
-                # calculate and print total elapsed time and estimated time left
-                end_time = time.time()
-                elapsed_time = end_time - first_task_start_time
-                time_per_tasks = elapsed_time / current_tasks
-                time_left = time_per_tasks * (len(tasks) - current_tasks)
-
-                pbar.set_postfix(
-                    elapsed=f"{elapsed_time:.2f}s", time_left=f"{time_left:.2f}"
-                )
-
-    if current_tasks == number_of_tasks:
-        atexit.register(tasks_done)
-        celery_worker.terminate()
+    distributaur.monitor_tasks(tasks)
